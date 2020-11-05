5a697ed0e6e77c5778fbabcf7d1b2837553b478d
kaspar.arme <kaspar.arme@reach-u.com>
2020-11-05 13:16:41 +0200

    Final solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+import { map, zoomToMarker, addToMap, clearMap } from "./map.js";
+
+let autocompleteResults = document.getElementById("js-autocomplete");
 let resultsContainer = document.getElementById("js-results-container");
 let searchResults = document.getElementById("js-results");
 let searchInput = document.getElementById("js-search");
 let clearButton = document.getElementById("js-clear");
+let autocompleteData = null;
 
+searchInput.addEventListener("input", (event) => {
+  fetchAutocompleteResults(event.target.value);
+});
 searchInput.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     fetchSearchResults(searchInput.value);
+  }
+});
+searchInput.addEventListener("blur", (event) => {
+  if (event.relatedTarget.classList.contains("autocomplete-item")) return;
+
+  autocompleteResults.innerHTML = "";
+});
+searchInput.addEventListener("focus", () => {
+  if (autocompleteData) {
+    createAutocompleteResults(autocompleteData);
   }
 });
 
@@ -21,23 +38,43 @@ document.getElementById("js-submit").addEventListener("click", () => {
 
 clearButton.addEventListener("click", () => {
   searchInput.value = "";
+  autocompleteData = null;
   clearAllResults();
 });
 
 let clearAllResults = () => {
+  autocompleteResults.innerHTML = "";
   searchResults.innerHTML = "";
   resultsContainer.classList.add("hidden");
   clearButton.classList.add("hidden");
 };
 
-let fetchSearchResults = (query) => {
-  fetch(`/poi?q=${query}`)
+let fetchAutocompleteResults = (query) => {
+  fetch(`/poi?q=${query}&limit=5`)
+    .then((res) => res.json())
+    .then((data) => {
+      autocompleteResults.innerHTML = "";
+      if (data.features) {
+        autocompleteData = data.features;
+        createAutocompleteResults(data.features);
+      }
+    });
+};
+
+let fetchSearchResults = (query, zoomToFirst = false) => {
+  let { lat, lng } = map.getCenter();
+  fetch(`/poi?q=${query}&lat=${lat}&lon=${lng}`)
     .then((res) => res.json())
     .then((data) => {
       clearAllResults();
+      clearMap();
       searchInput.blur();
       if (data.features && data.features.length > 0) {
         createSearchResults(data.features);
+        addToMap(data);
+        if (zoomToFirst) {
+          zoomToMarker(data.features[0]);
+        }
       } else {
         let message = document.createElement("p");
         message.classList.add("no-results");
@@ -48,6 +85,26 @@ let fetchSearchResults = (query) => {
         clearButton.classList.remove("hidden");
       }
     });
+};
+
+let createAutocompleteResults = (features) => {
+  let buttons = features.map((feature) => {
+    let { name, level0, level1, level2 } = feature.properties;
+    let button = document.createElement("button");
+    button.classList.add("autocomplete-item");
+    button.innerHTML = `
+          <div>${name}</div>
+          <span>${level0}${level1 ? `, ${level1}` : ""}${level2 ? `, ${level2}` : ""}</span>
+    `;
+    button.addEventListener("click", () => {
+      searchInput.value = feature.properties.name;
+      fetchSearchResults(feature.properties.name, true);
+    });
+
+    return button;
+  });
+
+  autocompleteResults.append(...buttons);
 };
 
 let createSearchResults = (features) => {
@@ -61,6 +118,9 @@ let createSearchResults = (features) => {
           <span>${level0}${level1 ? `, ${level1}` : ""}${level2 ? `, ${level2}` : ""}</span>
           <svg viewBox="0 0 24 24"><path fill="currentColor" d="M8.59 16.58L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.42z"/></svg>
     `;
+    button.addEventListener("click", () => {
+      zoomToMarker(feature);
+    });
 
     return button;
   });
---
+++ src/map.js
@@ -0,0 +1,42 @@
+export let map = L.map("js-map", {
+  center: [-1.29, 36.8],
+  zoom: 12,
+  zoomControl: false
+});
+
+new L.Control.Zoom({ position: "bottomright" }).addTo(map);
+
+L.tileLayer("/tile/{z}/{x}/{y}.png", {}).addTo(map);
+
+let mapMarkers = null;
+
+export let addToMap = (collection) => {
+  let layerGroup = new L.LayerGroup();
+  layerGroup.addTo(map);
+  let markers = L.geoJSON(collection, {
+    pointToLayer: (feature, latlng) => {
+      return L.marker(latlng, {
+        riseOnHover: true,
+        icon: new L.DivIcon({
+          className: "custom-marker",
+          html: `<svg viewBox="0 0 24 24"><path fill="currentColor" d="M12 11.5A2.5 2.5 0 019.5 9 2.5 2.5 0 0112 6.5 2.5 2.5 0 0114.5 9a2.5 2.5 0 01-2.5 2.5M12 2a7 7 0 00-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 00-7-7z"/></svg>`
+        }),
+        title: feature.properties.name
+      });
+    }
+  });
+  layerGroup.addLayer(markers);
+
+  mapMarkers = layerGroup;
+};
+
+export let zoomToMarker = (feature) => {
+  const [lng, lat] = feature.geometry.coordinates;
+  map.flyTo(L.latLng(lat, lng), 17);
+};
+
+export let clearMap = () => {
+  if (mapMarkers) {
+    map.removeLayer(mapMarkers);
+  }
+};
